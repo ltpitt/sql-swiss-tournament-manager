d0237f3522f0e53efeccc51ca7395580e66a9526
Davide Nastri <d.nastri@gmail.com>
2015-05-25 01:43:27 +0200

    Changed db structure to allow more than one tournament

--- tournament.sql
+++ tournament.sql
@@ -1,7 +1,10 @@
 -- Table definitions for the tournament project.
 
 -- Remove all data, if present
-DROP DATABASE IF EXISTS tournament;
+
+\c vagrant
+
+DROP DATABASE tournament;
 
 CREATE DATABASE tournament ENCODING 'utf8';
 
@@ -10,7 +13,9 @@ CREATE DATABASE tournament ENCODING 'utf8';
 CREATE TABLE t_players (
 
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL
+    name TEXT NOT NULL,
+    surname TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL
 
     );
 
@@ -54,27 +59,37 @@ CREATE TABLE t_tournaments_players (
 
   );
 
-CREATE VIEW v_matches_won_per_player AS
-    SELECT t_players.id, t_players.name,
-    COUNT(t_matches.id_winner) AS total_matches_won
-    FROM t_players
-    LEFT JOIN t_matches
-    ON t_players.id = t_matches.id_winner
-    GROUP BY t_players.id
-    ORDER BY total_matches_won DESC;
 
-CREATE VIEW v_matches_lost_per_player AS
-    SELECT t_players.id, t_players.name,
-    COUNT(t_matches.id_loser) AS total_matches_lost
-    from t_players LEFT JOIN t_matches
-    ON t_players.id = t_matches.id_loser
-    GROUP BY t_players.id
+CREATE VIEW v_players_per_tournament AS
+    SELECT t_players.id as id_player, t_players.name as player_name, t_tournaments_players.id_tournament
+    FROM t_players
+    JOIN t_tournaments_players
+    ON t_players.id = t_tournaments_players.id_player;
+
+
+CREATE VIEW v_matches_lost_per_player_per_tournament AS
+    SELECT v_players_per_tournament.id_player, v_players_per_tournament.player_name, v_players_per_tournament.id_tournament, COUNT(t_matches.id_loser) AS total_matches_lost
+    FROM v_players_per_tournament
+    LEFT JOIN t_matches ON v_players_per_tournament.id_player = t_matches.id_loser and v_players_per_tournament.id_tournament = t_matches.id_tournament
+    GROUP BY v_players_per_tournament.id_player, v_players_per_tournament.player_name, v_players_per_tournament.id_tournament
     ORDER BY total_matches_lost DESC;
 
+CREATE VIEW v_matches_won_per_player_per_tournament AS
+    SELECT v_players_per_tournament.id_player, v_players_per_tournament.player_name, v_players_per_tournament.id_tournament, COUNT(t_matches.id_winner) AS total_matches_won
+    FROM v_players_per_tournament
+    LEFT JOIN t_matches ON v_players_per_tournament.id_player = t_matches.id_winner AND v_players_per_tournament.id_tournament = t_matches.id_tournament
+    GROUP BY v_players_per_tournament.id_player, v_players_per_tournament.player_name, v_players_per_tournament.id_tournament
+    ORDER BY total_matches_won DESC;
+
 CREATE VIEW v_total_stats AS
-    SELECT v_matches_won_per_player.id, v_matches_won_per_player.name,
-           v_matches_won_per_player.total_matches_won AS total_matches_won,
-           v_matches_lost_per_player.total_matches_lost AS total_matches_lost,
-           v_matches_won_per_player.total_matches_won + v_matches_lost_per_player.total_matches_lost as total_matches_played
-    FROM v_matches_won_per_player, v_matches_lost_per_player
-    WHERE v_matches_won_per_player.id = v_matches_lost_per_player.id;
+    SELECT v_matches_won_per_player_per_tournament.id_player as id, v_matches_won_per_player_per_tournament.player_name as name,
+           v_matches_won_per_player_per_tournament.total_matches_won as wins,
+           v_matches_lost_per_player_per_tournament.total_matches_lost as losses,
+           v_matches_won_per_player_per_tournament.id_tournament
+    FROM v_matches_won_per_player_per_tournament, v_matches_lost_per_player_per_tournament
+    WHERE v_matches_won_per_player_per_tournament.id_player = v_matches_lost_per_player_per_tournament.id_player AND v_matches_won_per_player_per_tournament.id_tournament = v_matches_lost_per_player_per_tournament.id_tournament
+    GROUP BY v_matches_won_per_player_per_tournament.id_player, v_matches_won_per_player_per_tournament.player_name,
+           v_matches_won_per_player_per_tournament.total_matches_won,
+           v_matches_lost_per_player_per_tournament.total_matches_lost,
+           v_matches_won_per_player_per_tournament.id_tournament;
+
