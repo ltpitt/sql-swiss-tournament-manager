066b199c7a8156e6068086fe82c4f43dff952e78
Davide Nastri <d.nastri@gmail.com>
2015-05-27 01:23:39 +0200

    Restored original test.py file

--- test.py
+++ test.py
@@ -4,7 +4,6 @@
 
 from tournament import *
 
-
 def testDeleteMatches():
     deleteMatches()
     print "1. Old matches can be deleted."
@@ -136,3 +135,5 @@ if __name__ == '__main__':
     testReportMatches()
     testPairings()
     print "Success!  All tests pass!"
+
+
